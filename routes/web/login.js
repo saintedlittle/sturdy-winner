7d1862111f93f5ba0e99ab33175bbbc76c0ed579
saintedlittle <l.kapitman@gmail.com>
2024-04-20 13:01:40 -0700

    Заготовка работы с логином.

--- routes/web/login.js
+++ routes/web/login.js
@@ -5,4 +5,14 @@ router.get('/', function(req, res, next) {
     res.render('login', {});
 });
 
+router.post('/login', function(req, res, next) {
+    // Здесь вы можете обрабатывать отправленные данные формы
+    const email = req.body.email;
+    const password = req.body.password;
+
+
+
+    res.redirect('/');
+});
+
 module.exports = router;
